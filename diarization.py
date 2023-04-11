98f510d03b9ccc0b4ff1cb84087fedb3ee5a744d
Rajeshshashank <75923289+Rajeshshashank@users.noreply.github.com>
2023-04-11 13:07:49 +0530

    Update diarization.py

--- diarization.py
+++ diarization.py
@@ -2,5 +2,7 @@ from pyAudioAnalysis import audioSegmentation
 
 def diarize_speakers(audio_file):
     segments = audioSegmentation.speaker_diarization(audio_file)
+    result_str = ""
     for segment in segments:
-        print('Speaker %d: %d - %d' % (segment['speaker'], segment['start'], segment['end']))
+        result_str += 'Speaker %d: %d - %d\n' % (segment['speaker'], segment['start'], segment['end'])
+    return result_str
