2e912d9bc692fbfbfc77ba7f7ce6813fadd5e134
Rajesh Shashank <75923289+Rajeshshashank@users.noreply.github.com>
2023-04-12 13:55:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import io
 import numpy as np
 import librosa
 import webrtcvad
@@ -12,22 +13,35 @@ from diarization import diarize_speakers
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/process_audio', methods=['POST'])
 def process_audio():
-    audio_file = request.files['audio']
+    # extract the uploaded file from the request object
+    file = request.files['audio']
 
-    # convert audio file to WAV format
-    audio_data = AudioSegment.from_file(audio_file)
-    audio_wav = audio_data.export(format='wav')
+    # convert the file data to a format that can be read by librosa
+    audio_data = AudioSegment.from_file(file)
+    audio_data = audio_data.set_frame_rate(16000)
+    audio_data = audio_data.set_channels(1)
+    audio_data.export('temp.wav', format='wav')
+    rate, wav_data = wavfile.read('temp.wav')
 
     # perform speaker diarization
-    results = diarize_speakers(audio_wav)
+    results = diarize_speakers(wav_data)
 
-    return jsonify(results)
+    # format the results as a string
+    result_str = ""
+    for i, speaker in enumerate(results):
+        result_str += f"Speaker {i + 1}: {speaker}\n"
+
+    # return the results as a JSON object
+    return jsonify({'results': result_str})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
